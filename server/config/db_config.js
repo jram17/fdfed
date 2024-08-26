3c0aa1f1aa10866fa3338ef66856d86f89d4951d
inthrakumar <inthrak04@gmail.com>
2024-08-26 09:26:31 +0530

    bug fix in db connection

--- server/config/db_config.js
+++ server/config/db_config.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-module.exports = () => {
-    mongoose.connect(`${process.env.MONGO_URI}`);
-    console.log('Connected to Mongodb database');
+module.exports = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Connected to Mongodb database`);
+
+
+    } catch (error) {
+        console.log(`Failed to connect to Mongodb`);
+    }
 }
--- server/server.js
+++ server/server.js
@@ -1,16 +1,13 @@
 const app = require(".");
 const connect = require("./config/db_config");
-
-async function initialize() {
+const initialize = async () => {
     try {
         await connect();
         app.listen(5000, () => {
-            console.log(" Server started on port 5000");
+            console.log("Server started on port 5000");
         });
-
     } catch (error) {
-        console.error("Failed to start the server:", error);
-        process.exit(1);
+        console.log("Error: ", error);
     }
 }
 
