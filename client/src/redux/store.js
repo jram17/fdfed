00f763bcb59353cf394fd4a20605369117ab5d21
inthrakumar <inthrak04@gmail.com>
2024-09-13 13:38:39 +0530

    redux persist bug fix

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,21 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist"
+import { persistStore, persistReducer } from "redux-persist";
 import authSlice from "./slice/authSlice";
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from "@reduxjs/toolkit";
+
 const persistConfig = {
     key: 'store',
     version: '1',
-    storage
-}
+    storage,
+};
+
 const reducer = combineReducers({
-    auth: authSlice
+    auth: authSlice,
+});
 
-})
+const persistedReducer = persistReducer(persistConfig, reducer);
 
-const persistedReducer = persistReducer(persistConfig, reducer)
 const store = configureStore({
-    reducer: persistedReducer
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+            },
+        }),
 });
 
 export default store;
