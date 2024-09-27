d8590dadcc01e9d3f92fbb6bf5e9e45896211971
inthrakumar <inthrak04@gmail.com>
2024-09-27 13:37:18 +0530

    basic setup for admin tables

--- client/src/components/RoomDetails.jsx
+++ client/src/components/RoomDetails.jsx
@@ -309,7 +309,7 @@ function RoomDetails({ apartment_id }) {
         </div>
       )}
       {isModal && (
-        <div className="fixed top-[25%]ft-0 right-0 flex justify-center z-50">
+        <div className="fixed top-1/4   flex justify-center z-50">
           <div className="relative bg-white p-4 rounded-lg shadow-lg">
             <span
               className="absolute top-2 right-2 cursor-pointer"
